e3201121024a87c8db5d1e720adeec442288b226
Bero <berislav.grgicak@gmail.com>
2024-06-10 10:14:20 +0000

    Convert to blocks

--- playground-markdown/playground-markdown.js
+++ playground-markdown/playground-markdown.js
@@ -3,6 +3,9 @@
   if (!window.playgroundMarkdown.markdown) {
     return;
   }
+
+  await import('../blocky-formats/vendor/commonmark.min.js');
+  const { markdownToBlocks, blocks2markdown } = await import('../blocky-formats/src/markdown.js');
   for (let file of window.playgroundMarkdown.markdown) {
     console.log("file", file);
     fetch("/wp-json/wp/v2/posts", {
