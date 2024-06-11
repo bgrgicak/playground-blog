902d70e226eab8cf4075e5475bf900313c97c5da
Bero <berislav.grgicak@gmail.com>
2024-06-11 05:55:51 +0000

    Test

--- playground-markdown/playground-markdown.js
+++ playground-markdown/playground-markdown.js
@@ -12,6 +12,9 @@
   );
 
   for (let file of window.playgroundMarkdown.markdown) {
+    console.log(file.content);
+    console.log(markdownToBlocks(file.content));
+
     const content = markdownToBlocks(file.content).map((block) => ({
       ...block,
       blockName: block.name,
@@ -36,6 +39,6 @@
   }
 
   if (window.location.pathname !== "/category/uncategorized/") {
-    window.open("/category/uncategorized/", "_self");
+    //window.open("/category/uncategorized/", "_self");
   }
 })();
